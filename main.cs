93f253d75c161564d9f8e5a8295f6f8bedb51094
dnadglowski <156368149+dnadglowski@users.noreply.github.com>
2024-05-13 19:08:10 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Oh, well imagine /");
+    Console.WriteLine ("As I'm pacing the pews in a church corridor /");
+    Console.WriteLine ("And I can't help but to hear /");
+    Console.WriteLine ("No, I can't help but to hear an exchanging of words..." + Environment.NewLine);
+    Console.WriteLine ("Oh-oh-oh-oh-oh,");
+    Console.WriteLine ("oh-oh-oh-oh,");
+    Console.WriteLine ("oh-oh-oh /");
+    Console.WriteLine ("Caught in a bad romance..." + Environment.NewLine);
+    Console.WriteLine ("Coming out of my cage");
+    Console.WriteLine ("And I've been doing just fine");
+    Console.WriteLine ("Gotta gotta be down");
+    Console.WriteLine ("Because I want it all" + Environment.NewLine);
+    Console.WriteLine ("Now, this is a story all about how");
+    Console.WriteLine ("My life got flipped-turned upside down");
+    Console.WriteLine ("And I'd like to take a minute");
+    Console.WriteLine ("Just sit right there");
+    Console.WriteLine ("I'll tell you how I became the prince of a town called Bel-Air");
+  }
+}
